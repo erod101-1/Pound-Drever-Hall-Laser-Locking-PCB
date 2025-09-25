0f1d05324e0c685ba2e0176ea96a394de34ef806
Eli Rodrigues <66928418+erod101-1@users.noreply.github.com>
2025-09-24 21:10:31 -0700

    cleanup

--- Simulations/pdh.py
+++ Simulations/pdh.py
@@ -2,53 +2,54 @@ from dataclasses import dataclass, field
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-class CONSTANTS:
-    SPEED_OF_LIGHT = 3e8 # speed of light
+SPEED_OF_LIGHT = 3e8 # speed of light
 
 @dataclass
-class laser(CONSTANTS):
+class laser:
     l_0: float
     P_c: float
     f_c: float = field(init=False)      # Hz
     omega_c: float = field(init=False)  # rad/s
     def __post_init__(self):
-        self.f_c = self.SPEED_OF_LIGHT / self.l_0
+        self.f_c = SPEED_OF_LIGHT / self.l_0
         self.omega_c = 2*np.pi * self.f_c
         
 
 @dataclass
-class eom(CONSTANTS):
+class eom:
     f_m : float # modulation frequency
     omega_m : float = field(init=False) # sideband angular frequency
     P_s : float # side band power
     def __post_init__(self):
         self.omega_m = 2*np.pi*self.f_m
+        
 @dataclass 
-class cavity(CONSTANTS):
+class cavity:
     r : float  # reflection coefficient
     L : float  # length of the cavity
     fsr : float = field(init=False) # free spectral range
     def __post_init__(self):
-        self.fsr = self.SPEED_OF_LIGHT/(2*self.L)
+        self.fsr = SPEED_OF_LIGHT/(2*self.L)
 
-class ReflectionCoeff(cavity):
-    def __init__(self,**kwargs):
-        c =kwargs.get('cavity',cavity(r=0.95, L=0.02))
-        super().__init__(r=c.r,L=c.L)
-
+    def reflection_coefficient(self,omega : np.linspace) -> float:
+        n = self.r*(np.exp(1j*omega/self.fsr)-1)
+        d = 1 - self.r*self.r*np.exp(1j*omega/self.fsr)
+        return n/d
+    
     # get reflection coeffient (from black paper)
     def reflection_coefficient(self,omega : np.linspace) -> float:
         n = self.r*(np.exp(1j*omega/self.fsr)-1)
         d = 1 - self.r*self.r*np.exp(1j*omega/self.fsr)
         return n/d
 
-class ReflectedPower(ReflectionCoeff):
+
+
+class ReflectedPower:
     def __init__(self,**kwargs):
-        super().__init__(**kwargs)
         
+        self.cavity = kwargs.get('cavity',cavity(r=0.95, L=0.02))
         self.laser = kwargs.get('laser',laser(l_0=1550e-9, P_c=5e-3))
-        self.eom = kwargs.get('eom',eom(f_m=20e6, P_s=0.5e-3))
+        self.eom = kwargs.get('eom',eom(f_m=0.1e9, P_s=0.5e-3))
         self.omega_space = np.linspace(self.laser.omega_c-3*self.eom.omega_m, \
                                        self.laser.omega_c+3*self.eom.omega_m, \
                                        int(1e6))
@@ -58,9 +59,9 @@ class ReflectedPower(ReflectionCoeff):
         delta = self.omega_space - self.laser.omega_c
 
         # Reflection coefficients at carrier and sidebands
-        F0 = self.reflection_coefficient(delta)
-        Fp = self.reflection_coefficient(delta + self.eom.omega_m)  # Omega + Omega
-        Fm = self.reflection_coefficient(delta - self.eom.omega_m)  # Omega − Omega
+        F0 = self.cavity.reflection_coefficient(delta)
+        Fp = self.cavity.reflection_coefficient(delta + self.eom.omega_m)  # Omega + Omega
+        Fm = self.cavity.reflection_coefficient(delta - self.eom.omega_m)  # Omega − Omega
 
         Pc = self.laser.P_c
         Ps = self.eom.P_s
@@ -75,15 +76,23 @@ class ReflectedPower(ReflectionCoeff):
         t3 = 2 * np.sqrt(Pc * Ps) * (np.real(A) * np.cos(Omega * t) + np.imag(A) * np.sin(Omega * t))
 
         return t1 + t2 + t3
+    
+    def time_averaged_power(self) -> float:
+        delta = rp.omega_space - rp.laser.omega_c
+        F0 = rp.cavity.reflection_coefficient(delta)
+        Fp = rp.cavity.reflection_coefficient(delta + rp.eom.omega_m)
+        Fm = rp.cavity.reflection_coefficient(delta - rp.eom.omega_m)
 
+        return (rp.laser.P_c*np.abs(F0)**2 + rp.eom.P_s*(np.abs(Fp)**2 + np.abs(Fm)**2))
+        
     def error_signal(self, demod_phase=np.pi/2):
         # symmetric detuning grid that includes 0 exactly
         delta = self.omega_space - self.laser.omega_c
 
         # cavity responses at carrier and sidebands
-        F0 = self.reflection_coefficient(delta)
-        Fp = self.reflection_coefficient(delta + self.eom.omega_m)
-        Fm = self.reflection_coefficient(delta - self.eom.omega_m)
+        F0 = self.cavity.reflection_coefficient(delta)
+        Fp = self.cavity.reflection_coefficient(delta + self.eom.omega_m)
+        Fm = self.cavity.reflection_coefficient(delta - self.eom.omega_m)
 
         # PDH baseband after mix + LPF
         Pc, Ps = self.laser.P_c, self.eom.P_s
@@ -91,22 +100,15 @@ class ReflectedPower(ReflectionCoeff):
         Verr = 2*np.sqrt(Pc*Ps) * (np.real(A)*np.cos(demod_phase) + np.imag(A)*np.sin(demod_phase))
         return delta, Verr  
 
+
 # PDH error signal 
 rp = ReflectedPower()
 
-# Time-averaged reflected power (no RF beat term)
-delta = rp.omega_space - rp.laser.omega_c
-F0 = rp.reflection_coefficient(delta)
-Fp = rp.reflection_coefficient(delta + rp.eom.omega_m)
-Fm = rp.reflection_coefficient(delta - rp.eom.omega_m)
-P_avg = (rp.laser.P_c*np.abs(F0)**2
-         + rp.eom.P_s*(np.abs(Fp)**2 + np.abs(Fm)**2))
-
-delta, Verr = rp.error_signal(demod_phase=np.pi/2)
+delta, Verr = rp.error_signal(demod_phase=-np.pi/2)
 
 plt.figure(figsize=(9,6))
 plt.subplot(2,1,1)
-plt.plot(delta, P_avg)
+plt.plot(delta, rp.time_averaged_power())
 plt.title("Reflected Power (time-averaged) vs Detuning")
 plt.ylabel("Power (W)")
 plt.grid(True)
