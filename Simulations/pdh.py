61afc804201270c2a7a00864452d1ffe0327f562
Eli Rodrigues <66928418+erod101-1@users.noreply.github.com>
2025-09-25 21:27:05 -0700

    refactor

--- Simulations/pdh.py
+++ Simulations/pdh.py
@@ -6,15 +6,16 @@ SPEED_OF_LIGHT = 3e8 # speed of light
 
 @dataclass
 class laser:
-    l_0: float
-    P_c: float
-    f_c: float = field(init=False)      # Hz
+    P_c : float
+    f_c_THz : float # THz
+    f_c_Hz : float = field(init=False)
+    l_0: float  = field(init=False) 
     omega_c: float = field(init=False)  # rad/s
     def __post_init__(self):
-        self.f_c = SPEED_OF_LIGHT / self.l_0
-        self.omega_c = 2*np.pi * self.f_c
-        
-
+        self.f_c_Hz = self.f_c_THz * 1e12 # convert THz to Hz
+        self.l_0 = SPEED_OF_LIGHT / self.f_c_Hz 
+        self.omega_c = 2*np.pi * self.f_c_Hz # 2*pi*Hz
+    
 @dataclass
 class eom:
     f_m : float # modulation frequency
@@ -23,6 +24,7 @@ class eom:
     def __post_init__(self):
         self.omega_m = 2*np.pi*self.f_m
         
+
 @dataclass 
 class cavity:
     r : float  # reflection coefficient
@@ -42,73 +44,65 @@ class cavity:
         d = 1 - self.r*self.r*np.exp(1j*omega/self.fsr)
         return n/d
 
+# Laser... -
+mcavity = cavity(r=0.95, L=0.02)
+mlaser = laser(P_c=1e-3,f_c_THz=193.15)
+meom = eom(f_m=0.1e9, P_s=0.5e-3)
 
-
-class PDH:
-    def __init__(self,**kwargs):
-        
-        self.cavity = kwargs.get('cavity',cavity(r=0.95, L=0.02))
-        self.laser = kwargs.get('laser',laser(l_0=1550e-9, P_c=5e-3))
-        self.eom = kwargs.get('eom',eom(f_m=0.1e9, P_s=0.5e-3))
-        self.omega_space = np.linspace(self.laser.omega_c-3*self.eom.omega_m, \
-                                       self.laser.omega_c+3*self.eom.omega_m, \
+omega_space = np.linspace(mlaser.omega_c-3*meom.omega_m, \
+                                       mlaser.omega_c+3*meom.omega_m, \
                                        int(1e6))
-        
-    def reflected_power(self):
-        # Detuning grid relative to the carrier
-        delta = self.omega_space - self.laser.omega_c
-
-        # Reflection coefficients at carrier and sidebands
-        F0 = self.cavity.reflection_coefficient(delta)
-        Fp = self.cavity.reflection_coefficient(delta + self.eom.omega_m)  # Omega + Omega
-        Fm = self.cavity.reflection_coefficient(delta - self.eom.omega_m)  # Omega − Omega
-
-        Pc = self.laser.P_c
-        Ps = self.eom.P_s
-        Omega  = self.eom.omega_m
-
-        # refer to black paper 
-        t1 = Pc * (np.abs(F0) ** 2)
-        t2 = Ps * ((np.abs(Fp) ** 2) + (np.abs(Fm) ** 2))
-
-        A = F0 * np.conj(Fp) - np.conj(F0) * Fm
-        t = 0.0
-        t3 = 2 * np.sqrt(Pc * Ps) * (np.real(A) * np.cos(Omega * t) + np.imag(A) * np.sin(Omega * t))
-
-        return t1 + t2 + t3
-    
-    def time_averaged_power(self) -> float:
-        delta = rp.omega_space - rp.laser.omega_c
-        F0 = rp.cavity.reflection_coefficient(delta)
-        Fp = rp.cavity.reflection_coefficient(delta + rp.eom.omega_m)
-        Fm = rp.cavity.reflection_coefficient(delta - rp.eom.omega_m)
-
-        return (rp.laser.P_c*np.abs(F0)**2 + rp.eom.P_s*(np.abs(Fp)**2 + np.abs(Fm)**2))
-        
-    def error_signal(self, demod_phase=np.pi/2):
-        # symmetric detuning grid that includes 0 exactly
-        delta = self.omega_space - self.laser.omega_c
-
-        # cavity responses at carrier and sidebands
-        F0 = self.cavity.reflection_coefficient(delta)
-        Fp = self.cavity.reflection_coefficient(delta + self.eom.omega_m)
-        Fm = self.cavity.reflection_coefficient(delta - self.eom.omega_m)
-
-        # PDH baseband after mix + LPF
-        Pc, Ps = self.laser.P_c, self.eom.P_s
-        A = F0*np.conj(Fp) - np.conj(F0)*Fm
-        Verr = 2*np.sqrt(Pc*Ps) * (np.real(A)*np.cos(demod_phase) + np.imag(A)*np.sin(demod_phase))
-        return delta, Verr  
 
 
-# PDH error signal 
-rp = PDH()
+def reflected_power(t):
+    # Detuning grid relative to the carrier
+    delta = omega_space - mlaser.omega_c
+    # Reflection coefficients at carrier and sidebands
+    F0 = mcavity.reflection_coefficient(delta)
+    Fp = mcavity.reflection_coefficient(delta + meom.omega_m)  # Omega + Omega
+    Fm = mcavity.reflection_coefficient(delta - meom.omega_m)  # Omega − Omega
+    Pc = mlaser.P_c
+    Ps = meom.P_s
+    Omega  = meom.omega_m
+    # refer to black paper 
+    t1 = Pc * (np.abs(F0) ** 2)
+    t2 = Ps * ((np.abs(Fp) ** 2) + (np.abs(Fm) ** 2))
+    A = F0 * np.conj(Fp) - np.conj(F0) * Fm
+    t3 = 2 * np.sqrt(Pc * Ps) * (np.real(A) * np.cos(Omega * t) + np.imag(A) * np.sin(Omega * t))
+    return t1 + t2 + t3
 
-delta, Verr = rp.error_signal(demod_phase=-np.pi/2)
+def time_averaged_power() -> float:
+    delta = omega_space - mlaser.omega_c
+    F0 = mcavity.reflection_coefficient(delta)
+    Fp = mcavity.reflection_coefficient(delta + meom.omega_m)
+    Fm = mcavity.reflection_coefficient(delta - meom.omega_m)
+    return (mlaser.P_c*np.abs(F0)**2 + meom.P_s*(np.abs(Fp)**2 + np.abs(Fm)**2))
+
+
+def error_signal(demod_phase=np.pi/2):
+
+    delta = omega_space - mlaser.omega_c
+    Om = meom.omega_m
+
+    F0 = mcavity.reflection_coefficient(delta)
+    Fp = mcavity.reflection_coefficient(delta + Om)
+    Fm = mcavity.reflection_coefficient(delta - Om)
+
+    Pc, Ps = mlaser.P_c, meom.P_s
+
+    A = F0*np.conj(Fp) - np.conj(F0)*Fm
+
+    Verr = 2*np.sqrt(Pc*Ps) * (
+        np.real(A)*np.cos(demod_phase) + np.imag(A)*np.sin(demod_phase)
+    )
+    return delta, Verr
+
+
+delta, Verr = error_signal()
 
 plt.figure(figsize=(9,6))
 plt.subplot(2,1,1)
-plt.plot(delta, rp.time_averaged_power())
+plt.plot(delta, time_averaged_power())
 plt.title("Reflected Power (time-averaged) vs Detuning")
 plt.ylabel("Power (W)")
 plt.grid(True)
